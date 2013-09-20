e5b43cfe02cb584c1b95d867939b9d8309b1e0d2
ankur garha <speedyankur@gmail.com>
2013-09-20 13:05:10 +0530

    Added submit functionality

--- TrantorBugTracker/app/controllers/issueReporterTab.js
+++ TrantorBugTracker/app/controllers/issueReporterTab.js
@@ -1,4 +1,5 @@
 var currentSelctedDateField;
+
 function showPickerView(e) {
 	if (OS_IOS)
 		e.source.blur();
@@ -14,15 +15,15 @@ function showPickerView(e) {
 				} else {
 					Ti.API.info('User selected date: ' + e.value);
 					var selectedDateValue = e.value.toDateString();
-					
+
 					switch (currentSelctedDateField) {
 
-					case "DATE_IDENTIFIED":
-						$.dateIdentified.value = selectedDateValue;
-						break;
-					case "DATE_RESOLVED":
-						$.dateResolved.value = selectedDateValue;
-						break;
+						case "DATE_IDENTIFIED":
+							$.dateIdentified.value = selectedDateValue;
+							break;
+						case "DATE_RESOLVED":
+							$.dateResolved.value = selectedDateValue;
+							break;
 
 					}
 				}
@@ -35,15 +36,18 @@ function hidePickerView(e) {
 	if (OS_IOS)
 		$.pickerView.animate(slide_out);
 	var selectedDateValue = $.picker.value.toDateString();
-	Ti.API.info("picker value=" + selectedDateValue)
+	Ti.API.info("picker value string=" + selectedDateValue)
+	Ti.API.info("picker value iso=" + $.picker.value.toISOString())
 	switch (currentSelctedDateField) {
 
-	case "DATE_IDENTIFIED":
-		$.dateIdentified.value = selectedDateValue;
-		break;
-	case "DATE_RESOLVED":
-		$.dateResolved.value = selectedDateValue;
-		break;
+		case "DATE_IDENTIFIED":
+			$.dateIdentified.value = selectedDateValue;
+			$.dateIdentified.data = $.picker.value.toISOString();
+			break;
+		case "DATE_RESOLVED":
+			$.dateResolved.value = selectedDateValue;
+			$.dateResolved.data = $.picker.value.toISOString();
+			break;
 
 	}
 }
@@ -54,3 +58,49 @@ var slide_in = Titanium.UI.createAnimation({
 var slide_out = Titanium.UI.createAnimation({
 	bottom : "-250"
 });
+
+function submit() {
+	if($.projectField.value == "")
+	{
+		alert("Enter Project Name");
+		$.projectField.focus();
+		return;
+	}
+	if($.descriptionField.value == "")
+	{
+		alert("Enter Description");
+		$.descriptionField.focus();
+		return;
+	}
+	if($.severityField.value == "")
+	{
+		alert("Enter Severity");
+		$.severityField.focus();
+		return;
+	}	
+	var data = {
+		"project" : $.projectField.value,
+		"Description" : $.descriptionField.value,
+		status : $.statusField.value,
+		severity : $.severityField.value,
+		mitigationPlan : $.mitigationField.value,
+		comments : $.commentsField.value
+	};
+	if ($.dateIdentified.data)
+		data["dateIdentified"] = {
+			"__type" : "Date",
+			"iso" : $.dateIdentified.data
+		}
+
+	if ($.dateResolved.data)
+		data["dateResolved"] = {
+			"__type" : "Date",
+			"iso" : $.dateResolved.data
+		}
+
+	Ti.API.info("data--" + JSON.stringify(data));
+	var dataToPost = JSON.parse(JSON.stringify(data));
+	Ti.API.info("data--" + dataToPost);
+	Alloy.Globals.dataAccesslayer.addNewIssue(JSON.stringify(data));
+
+}
--- TrantorBugTracker/app/lib/dataAccessLayer/Issue.js
+++ TrantorBugTracker/app/lib/dataAccessLayer/Issue.js
@@ -1,38 +1,74 @@
 exports.getAllIssues = function(callback) {
 
 	Alloy.Globals.progressBar.openIndicator("Please wait");
-	var xhr = Titanium.Network
-			.createHTTPClient({
-				ondatastream : function(e) {
-					Alloy.Globals.progressBar.setValue((Math
-							.round(e.progress * 100)));
-				},
-				onload : function(e) {
-					var responseData = JSON.parse(this.responseText);
-					callback(responseData);
-					Alloy.Globals.progressBar.closeIndicator();
+	var xhr = Titanium.Network.createHTTPClient({
+		ondatastream : function(e) {
+			Alloy.Globals.progressBar.setValue((Math.round(e.progress * 100)));
+		},
+		onload : function(e) {
+			var responseData = JSON.parse(this.responseText);
+			callback(responseData);
+			Alloy.Globals.progressBar.closeIndicator();
 
-				},
-				onerror : function(e) {
-					// Display error message
-					var errorAlert = Titanium.UI
-							.createAlertDialog({
-								title : 'Connection Error',
-								message : 'A problem has occured. Please make sure you have internet access and try again.',
-								buttonNames : [ 'OK' ],
-								cancel : 1
-							});
-					errorAlert.show();
-					Alloy.Globals.progressBar.closeIndicator();
-
-				}
+		},
+		onerror : function(e) {
+			// Display error message
+			var errorAlert = Titanium.UI.createAlertDialog({
+				title : 'Connection Error',
+				message : 'A problem has occured. Please make sure you have internet access and try again.',
+				buttonNames : ['OK'],
+				cancel : 1
 			});
+			errorAlert.show();
+			Alloy.Globals.progressBar.closeIndicator();
+
+		}
+	});
 	var url = "https://api.parse.com/1/classes/Issues/";
 	xhr.open('GET', url);
-	xhr.setRequestHeader('X-Parse-Application-Id',
-			'VpoxE6VBC7eT7LIJpEswgF7SLNqHPbPtOt5gih6V');
-	xhr.setRequestHeader('X-Parse-REST-API-Key',
-			'ewyhKvinwvAgrcygZnIggvfYqTghg2qQg63L8GzS');
+	xhr.setRequestHeader('X-Parse-Application-Id', 'VpoxE6VBC7eT7LIJpEswgF7SLNqHPbPtOt5gih6V');
+	xhr.setRequestHeader('X-Parse-REST-API-Key', 'ewyhKvinwvAgrcygZnIggvfYqTghg2qQg63L8GzS');
 	xhr.send();
 
 };
+exports.addNewIssue = function(data) {
+	
+	Alloy.Globals.progressBar.openIndicator("Submitting Issue");
+	var xhr = Titanium.Network.createHTTPClient({
+		ondatastream : function(e) {
+			Alloy.Globals.progressBar.setValue((Math.round(e.progress * 100)));
+		},
+		onload : function(e) {
+			var responseData = JSON.parse(this.responseText);
+			var errorAlert = Titanium.UI.createAlertDialog({
+				title : 'Issue Submitted successfully ',
+				message : 'Issue Submitted Successfully, You can see the same issue listed under All Issues Tab',
+				buttonNames : ['OK'],
+				cancel : 1
+			});
+			errorAlert.show();
+			Alloy.Globals.progressBar.closeIndicator();
+
+		},
+		onerror : function(e) {
+			// Display error message
+			Ti.API.info(JSON.stringify(e));
+			var errorAlert = Titanium.UI.createAlertDialog({
+				title : 'Connection Error',
+				message : 'A problem has occured. Please make sure you have internet access and try again.',
+				buttonNames : ['OK'],
+				cancel : 1
+			});
+			errorAlert.show();
+			Alloy.Globals.progressBar.closeIndicator();
+
+		}
+	});
+	var url = "https://api.parse.com/1/classes/Issues/";
+	xhr.open('POST', url);
+	xhr.setRequestHeader('X-Parse-Application-Id', 'VpoxE6VBC7eT7LIJpEswgF7SLNqHPbPtOt5gih6V');
+	xhr.setRequestHeader('X-Parse-REST-API-Key', 'ewyhKvinwvAgrcygZnIggvfYqTghg2qQg63L8GzS');
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.send(data);
+
+}; 
